c478a2e3290bb3da00b1a7c371194987226a3dd3
Oralita <oat431@gmail.com>
2020-09-10 18:06:24 +0700

    add more route in the admin

--- routes/admin.js
+++ routes/admin.js
@@ -2,4 +2,7 @@ const express = require('express');
 const router = express.Router();
 const memberList = require('../controller/memberList');
 router.get('/memberList',memberList.member);
+router.get('/:id/delete',memberList.deleteMember);
+router.get('/:id/edit',memberList.getUserEdit);
+router.post('/edited',memberList.EditUser);
 module.exports = router;
