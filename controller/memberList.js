ff671c96c87d18e5313e138dace58a406eee1a72
Oralita <oat431@gmail.com>
2020-09-10 18:05:07 +0700

    add delete and edit to user by admin

--- controller/memberList.js
+++ controller/memberList.js
@@ -9,3 +9,40 @@ exports.member = (req,res) =>{
         });
     });
 }
+exports.deleteMember = (req,res) =>{
+    const query = 'delete from usersdetails where user_id=?';
+    db.query(query,[req.params.id],(err, result)=>{
+        if(err){
+            throw err;
+        }
+        req.flash('success','delete success!')
+        res.redirect('/admin/memberList');
+    }) ;
+}
+exports.getUserEdit = (req,res) =>{
+    const user_id = req.params.id;
+    const query = 'select * from usersdetails where user_id=?';
+    db.query(query,[user_id],(err,result)=>{
+        if(err){
+            throw err;
+        }
+        res.render('adminEdit',{
+            userdata:result
+        });
+    });
+}
+exports.EditUser = (req,res) =>{
+    const user_id = req.body.userid;
+    const name = req.body.name;
+    const money = req.body.money;
+    const tier = req.body.tier;
+    const query = 'update usersdetails set user_name=?,user_balance=?,userTier=?' +
+        'where user_id=?';
+    db.query(query,[name,money,tier,user_id],(err,result)=>{
+        if(err){
+            throw err;
+        }
+        req.flash('success','reader data is updated');
+        res.redirect('/admin/memberList');
+    })
+}
