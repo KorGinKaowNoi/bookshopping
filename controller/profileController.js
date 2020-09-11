5d68a3c068ac6b8ae541da2431c16aefa64995e2
KorGinKaowNoi <59800914+KorGinKaowNoi@users.noreply.github.com>
2020-09-11 20:54:24 +0700

    Update profileController.js

--- controller/profileController.js
+++ controller/profileController.js
@@ -2,7 +2,12 @@ exports.getProfile = (req,res)=>{
     const query = 'select * from usersdetails where user_id=?'
     db.query(query,[[req.session.thisuser]],(err,result)=>{
         if(err) throw err;
-        console.log(result);
+
+        if(result[0].user_image===''){
+            result[0].user_image = 'defultPicture.png'
+            console.log(result[0])
+        }
+        console.log(result[0])
         res.render('profile',{userdata:result});
     })
 }
