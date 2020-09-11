639be6a5d00b680690d955609fc720c6be342a5f
KorGinKaowNoi <59800914+KorGinKaowNoi@users.noreply.github.com>
2020-09-11 20:50:42 +0700

    Add files via upload

--- controller/buyBookController.js
+++ controller/buyBookController.js
@@ -1,4 +1,5 @@
 let date = new Date();
+const email = require('../config/email');
 exports.checkAndBuy = (req,res)=>{
     let buyer={} ;
     let book ={};
@@ -9,7 +10,6 @@ exports.checkAndBuy = (req,res)=>{
         const query = 'select * from usersdetails where user_id=?'
         db.query(query,[data],async (err,result)=>{
             if(err) throw err;
-            //console.log(result[0])
             return await cb(result[0]);
         });
     }
@@ -21,7 +21,7 @@ exports.checkAndBuy = (req,res)=>{
         })
     };
     const getSeller = (data,cb)=>{
-        let query = 'select * from usersdetails where user_id=?'
+        let query = 'select * from usersdetails inner join reader using(user_id) where user_id=?'
         db.query(query,[data],async (err,result)=>{
             if(err) throw err;
             return await cb(result[0]);
@@ -39,7 +39,16 @@ exports.checkAndBuy = (req,res)=>{
             req.flash('danger','your current balance is not enough')
             res.redirect('/shopcart');
         }else{
-            const hasleft =  buyer.user_balance-book.book_price;
+            const tier = buyer.userTier;
+            console.log(tier)
+            let discount=1;
+            if(tier==='gold'){
+                discount=0.9;
+            }else if(tier==='silver'){
+                discount=0.95;
+            }
+            const hasleft =  buyer.user_balance-(book.book_price*discount);
+            const income = (book.book_price*discount);
             let query = 'update usersdetails set user_balance=? where user_id=?'
             db.query(query,[hasleft,user],(err)=>{
                 if(err) throw err;
@@ -48,13 +57,26 @@ exports.checkAndBuy = (req,res)=>{
                 seller = result;
             })
             setTimeout(()=>{
-                db.query(query,[book.book_price+seller.user_balance,seller.user_id],(err,result)=>{
+                db.query(query,[income+seller.user_balance,seller.user_id],(err,result)=>{
                     if(err) throw err;
                 })
                 query = 'update usersdetails set user_payment=? where user_id=?'
-                console.log(buyer.user_payment,book.book_price,buyer.user_id)
-                db.query(query,[buyer.user_payment+book.book_price,buyer.user_id],(err)=>{
+                db.query(query,[buyer.user_payment+income,buyer.user_id],(err)=>{
                     if(err) throw err;
+                    const checkPayment = 'select user_payment from usersdetails where user_id=?'
+                    db.query(checkPayment,[buyer.user_id],(error,result)=>{
+                        if(error) throw err;
+                        let update = 'update usersdetails set userTier=? where user_id=?'
+                        if(result[0].user_payment>3000){
+                            db.query(update,['silver',buyer.user_id],(err)=>{
+                                if(err) throw err;
+                            })
+                        }else if(result[0].user_payment>7000){
+                            db.query(update,['gold',buyer.user_id],(err)=>{
+                                if(err) throw err;
+                            })
+                        }
+                    })
                 })
                 query = 'update orderdetails set payment_date = ? where user_id=?'
                 const time = date.getDate()+'/'+date.getMonth()+1+'/'+date.getFullYear();
@@ -70,6 +92,19 @@ exports.checkAndBuy = (req,res)=>{
                     if(err) throw err;
                     req.flash('success','your new book has been store in your catalog now');
                     res.redirect('/users/'+user+'/mybook');
+                })
+                let  mailOptions = {
+                    from: 'bookshopProjectNoreply@gmail.com',
+                    to: seller.user_email,
+                    subject: 'order details',
+                    html: '<b>your order have been ordered</b>'
+                };
+                email.emailing.sendMail(mailOptions,(err,info)=>{
+                    if(err){
+                        console.log(err)
+                    }else{
+                        console.log(info)
+                    }
                 })
             },1500)
         }
