a511bb794fe09c94620d5744858f308380d7db56
Oralita <oat431@gmail.com>
2020-09-13 00:19:53 +0700

    add book module

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const memberList = require('../controller/memberList');
 router.get('/memberList',memberList.member);
+router.get('/bookList',memberList.book);
 router.get('/:id/delete',memberList.deleteMember);
 router.get('/:id/edit',memberList.getUserEdit);
 router.post('/edited',memberList.EditUser);
--- routes/shopcart.js
+++ routes/shopcart.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 const addshop = require('../controller/addShopController');
 const buybook = require('../controller/buyBookController');
-//router.get('/',addshop.showMarket);
+router.get('/',addshop.showMarket);
 router.get('/add/:id',addshop.addtocart);
 router.post('/add/',addshop.cartToMarket);
 router.post('/:id',buybook.checkAndBuy);
